05364c4153b928bc50fa3960562e1d773af6cb1d
FranciscoBuru <edi@CC201-31.itam.mx>
2018-01-25 17:54:45 -0600

    Bug Fixes

---
+++ src/poo/Alumno3.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author Francisco Aramburu
+ */
+public class Alumno3 {
+    
+    
+    private String nom, curp, nacim, nomMad, nomPad, nomTut, nivel;
+
+    public Alumno3() {
+    }
+
+    public Alumno3(String nom, String curp, String nacim, String nomMad, String nomPad, String nivel) {
+        this.nom = nom;
+        this.curp = curp;
+        this.nacim = nacim;
+        this.nomMad = nomMad;
+        this.nomPad = nomPad;
+        this.nivel = nivel;
+    }
+
+    public Alumno3(String nom, String curp, String nacim, String nomTut, String nivel) {
+        this.nom = nom;
+        this.curp = curp;
+        this.nacim = nacim;
+        this.nomTut = nomTut;
+        this.nivel = nivel;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
